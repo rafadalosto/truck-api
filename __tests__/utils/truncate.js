6e4e35e9c18e24efff6ed95eb822205a69318a4e
Rafael Dalosto <rafael@informant.com.br>
2020-03-01 19:09:39 -0300

    created util class to clean the database on every tests and avoid tests failing because of trash from  another test

---
+++ __tests__/utils/truncate.js
@@ -0,0 +1,9 @@
+const connection = require('../../src/database/index');
+const models = connection.models;
+
+module.exports = () => {
+    return Promise.all(Object.keys(models).map(key => {
+        return models[key].destroy({truncate: true, force: true});
+    }));
+}
+

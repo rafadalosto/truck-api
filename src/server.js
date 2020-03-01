29e7434ae8edfdde25612406a1f6231ac59e38d4
Rafael Dalosto <rafael@informant.com.br>
2020-03-01 19:06:58 -0300

    moved the express configuration to file app

--- src/server.js
+++ src/server.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const routes = require('./routes');
-
-require('./database')
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-
+const app = require('./app');
 
 app.listen(3333);
